f6871115a16789fe12c26b1ecbc5baac6b14dbe3
Esther <dijayst@gmail.com>
2025-02-12 13:36:09 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="page-container bg-white min-h-screen">
-      <nav className="nav-container border-b-[1px] flex items-center md:px-[2rem] px-[1rem] min-h-[4rem] border-gray-100">
-        <p className="text-purple-400 text-[1.3rem] font-semibold">
-          Voltis-Labs
-        </p>
+    <div className="page-container bg-white min-h-screen ">
+      <nav className="nav-container border-b-[1px] flex items-center md:px-[2rem] px-[1rem] min-h-[4rem] border-gray-100 justify-between">
+        
+        <img
+                  src="/Image/logo.jpeg" 
+                  alt="Icon"
+                  className=" w-28 h-18 "
+                />
+                <div className="flex ">
+                <div><a>About us</a></div> <div><a>Project</a></div> <div><a>Contact</a></div></div>
       </nav>
+      
     </div>
   );
 }
