3d94cd62e93d7ef12d7970aa79248916215bbde5
davidfayanju1 <davidfayanju01@gmail.com>
2025-02-12 15:10:59 +0100

    Some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,13 @@ export default function Home() {
   return (
     <div className="page-container bg-white min-h-screen">
       <nav className="nav-container border-b-[1px] flex items-center md:px-[2rem] px-[1rem] min-h-[4rem] border-gray-100 justify-between">
-        
-        <img
-                  src="/Image/logo.jpeg" 
-                  alt="Icon"
-                  className=" w-28 h-18 "
-                />
-                <ul className="flex ">
-                <li><Link href="/" >About us</Link></li> <li><Link href="/" >Project</Link></li> <li><Link href="/" >Contact</Link></li></ul>
+        <img src="/image/logo.jpeg" alt="Icon" className="w-28 h-18" />
+        <ul className="flex text-black items-center gap-3">
+          <Link href="/">About us</Link>
+          <Link href="/">Project</Link>
+          <Link href="/">Contact</Link>
+        </ul>
       </nav>
-      
     </div>
   );
 }
