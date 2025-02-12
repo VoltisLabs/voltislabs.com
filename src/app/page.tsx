e6aee85c968534951ec272cda58453b1404ee2ef
Esther <dijayst@gmail.com>
2025-02-12 10:48:23 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="my-6 mx-6" >
-     <p className="test-lg font-normal text-purple-400 ">Voltislabs</p>
+     <p className="test-lg font-normal  ">Voltislabs</p>
     </div>
   );
 }
