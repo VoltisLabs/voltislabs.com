2127a898200b86479b6710494ef8673bb0893973
Esther <dijayst@gmail.com>
2025-02-12 13:52:34 +0100

    add the logo in the nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="page-container bg-white min-h-screen ">
+    <div className="page-container bg-white min-h-screen">
       <nav className="nav-container border-b-[1px] flex items-center md:px-[2rem] px-[1rem] min-h-[4rem] border-gray-100 justify-between">
         
         <img
