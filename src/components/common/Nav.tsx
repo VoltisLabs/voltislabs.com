696d7dcaff25807ab74805bd8f803a243bdd3a99
davidfayanju1 <davidfayanju01@gmail.com>
2025-02-14 15:48:38 +0100

    css correction:UI

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -33,28 +33,30 @@ const Nav = ({ setToggle }: NavProps) => {
   ];
 
   return (
-    <div className="page-container border-b-[1px] border-gray-600 md:px-[4rem] px-[1rem] min-h-[6rem] w-full flex items-center justify-start bg-black">
-      <div className="logo-container cursor-pointer mr-[17rem]">
-        <Image
-          src={"/image/logo3.png"}
-          alt="company-logo"
-          width={100}
-          height={100}
-          className="hover:scale-90 transition-all ease-in-out delay-75"
-        />
-      </div>
+    <div className="page-container border-b-[1px] border-gray-600 md:px-[4rem] px-[1rem] min-h-[6rem] w-full flex items-center md:justify-start justify-between bg-black">
+      <div className="nav-inner-container min-w-[65%] flex items-center justify-between">
+        <div className="logo-container cursor-pointer mr-[17rem]">
+          <Image
+            src={"/image/logo3.png"}
+            alt="company-logo"
+            width={100}
+            height={100}
+            className="hover:scale-90 transition-all ease-in-out delay-75"
+          />
+        </div>
 
-      {/* desktop links */}
-      <nav className="nav-container md:flex gap-6 hidden items-center">
-        {links.map((item) => (
-          <div
-            key={item.name}
-            className="card-container hover:underline cursor-pointer text-white text-[1rem]"
-          >
-            {item.name}
-          </div>
-        ))}
-      </nav>
+        {/* desktop links */}
+        <nav className="nav-container md:flex gap-6 hidden items-center">
+          {links.map((item) => (
+            <div
+              key={item.name}
+              className="card-container hover:underline cursor-pointer text-white text-[1rem]"
+            >
+              {item.name}
+            </div>
+          ))}
+        </nav>
+      </div>
 
       <section
         onClick={() => setToggle(true)}
