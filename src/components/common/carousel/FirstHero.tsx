bb5041315f4ca5c1bbd2ab62cdd3363cc1fb748f
davidfayanju1 <davidfayanju01@gmail.com>
2025-02-18 16:30:48 +0100

    Styling bug fixes

--- src/components/common/carousel/FirstHero.tsx
+++ src/components/common/carousel/FirstHero.tsx
@@ -18,7 +18,7 @@ const FirstHero = () => {
         </div>
       </div>
       <div className="right-section h-full w-[50%]"></div>
-      <section className="text-container absolute md:top-[30%] top-[25%] left-0 md:px-0 px-[1rem] w-full flex items-center justify-center mx-auto">
+      <section className="text-container absolute md:top-[30%] top-[26.5%] left-0 md:px-0 px-[1rem] w-full flex items-center justify-center mx-auto">
         <div className="content-container mx-auto md:w-[50%]">
           <div className="image-container flex items-center md:mb-1 mb-[1.5rem] gap-2">
             <Image
