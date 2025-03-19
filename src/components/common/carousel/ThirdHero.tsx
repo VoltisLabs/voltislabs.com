2dde0b6c89ff28851f5dec17561182b6e05b0a76
Abdullahi Olawale Mukaila <ayomikail21@gmail.com>
2025-03-19 15:11:34 -0400

    ThirdHero.tsx

--- src/components/common/carousel/ThirdHero.tsx
+++ src/components/common/carousel/ThirdHero.tsx
@@ -4,36 +4,59 @@ import React from "react";
 
 const ThirdHero = () => {
   return (
-    <section className="hero-section-container h-[37rem] md:h-[42rem] flex flex-row items-center justify-center gap-2 w-full">
+    <section className="hero-section-container flex items-stretch w-full h-screen">
       
       {/* Left Section (Text + Button) */}
-      <div className="py-6 md:px-8 px-4 bg-[#FFF1F1] w-[40%] h-full flex flex-col justify-between">
+      <div className="py-6 px-4 md:px-8 bg-[#FFF1F1] w-1/2 h-full flex flex-col justify-between">
         
         {/* Content */}
-        <div className="mt-20 text-center">
-          <Image src="/icons/hero.svg" alt="hero-icon" width={100} height={100} className="mx-auto" />
-          <span className="block font-semibold text-black text-[1.7rem] md:text-[3rem]">
+        <div className="mt-10 md:mt-20 text-center">
+          <Image 
+            src="/icons/hero.svg" 
+            alt="hero-icon" 
+            width={100} 
+            height={100} 
+            className="mx-auto" 
+          />
+          <span className="block font-semibold text-black text-[1.5rem] md:text-[3rem] leading-tight">
             Rooted in Culture, Styled for Everyone.
           </span>
         </div>
 
-        {/* Button - Always at Bottom Left */}
-        <div className="self-start flex gap-2 p-2 border border-black rounded-md">
-          <Link href="/vmodel" className="text-black text-sm">
-            Learn More about VModel
-          </Link>
-          <Image src="/icons/arrow-dark.svg" alt="arrow-icon" width={20} height={20} />
-        </div>
-
+        {/* Button - Always at Bottom & Centered */}
+	<div className="mt-auto self-center flex items-center gap-2 p-2 border border-black rounded-md">
+  	<Link href="/vmodel" className="text-black text-sm">
+    	Visit Website
+  	</Link>
+  		<Image 
+    		src="/icons/arrow-dark.svg" 
+    		alt="arrow-icon" 
+    		width={20} 
+    		height={20} 
+  		/>
+	</div>
+	
       </div>
 
       {/* Right Section (Two Side-by-Side Images) */}
-      <div className="flex w-[60%] h-full">
-        <div className="h-full w-1/2">
-          <Image src="/image/hero4.jpeg" alt="hero-image-1" width={500} height={500} className="h-full w-full object-cover" />
+      <div className="w-1/2 h-full flex">
+        <div className="w-1/2 h-full">
+          <Image 
+            src="/image/hero4.jpeg" 
+            alt="hero-image-1" 
+            width={500} 
+            height={500} 
+            className="w-full h-full object-cover" 
+          />
         </div>
-        <div className="h-full w-1/2">
-          <Image src="/image/hero5.jpeg" alt="hero-image-2" width={500} height={500} className="h-full w-full object-cover" />
+        <div className="w-1/2 h-full">
+          <Image 
+            src="/image/hero5.jpeg" 
+            alt="hero-image-2" 
+            width={500} 
+            height={500} 
+            className="w-full h-full object-cover" 
+          />
         </div>
       </div>
 
