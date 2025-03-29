b919afb2f723d25ec725062fcb05491187fc54a8
rich <adukyerer@gmail.com>
2025-03-29 13:43:19 +0000

    Add new hero image and enhance layout in ThirdHero component

--- src/components/common/carousel/ThirdHero.tsx
+++ src/components/common/carousel/ThirdHero.tsx
@@ -1,38 +1,61 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import LearnMoreBtn from "../../UI/LearnMoreBtn";
 
 const ThirdHero = () => {
   return (
-    <section className="hero-section-container md:h-[42rem] h-[37rem] flex items-center relative">
-      
-      {/* Left Section (Text + Button) */}
-      <div className="left-section md:p-4 p-1 ml-[-18px] bg-[#FFF1F1] w-2/5 h-full flex flex-col justify-between items-center text-center">
-        
-        {/* Hero Icon & Text */}
-        <div className="mt-20">
-          <Image src="/icons/hero.svg" alt="hero-icon" width={100} height={100} className="mx-auto" />
-          <span className="block font-semibold text-black text-[1.5rem] md:text-[3rem] leading-tight">
-            Rooted in Culture, Styled for Everyone.
-          </span>
-        </div>
-
-        {/* Button - Bottom Left */}
-        <div className="self-start flex items-center gap-2 p-2 ml-5 border border-black rounded-md">
-          <Link href="/vmodel" className="text-black text-sm">
-            Visit Website
-          </Link>
-          <Image src="/icons/arrow-dark.svg" alt="arrow-icon" width={20} height={20} />
-        </div>
-
+    <section className="hero-section-container md:h-[42rem] h-[37rem] flex items-center justify-center relative overflow-hidden">
+      {/* Background Images */}
+      <div className="absolute inset-0 flex">
+        <Image
+          src="/image/hero4.jpeg"
+          alt="hero-image-1"
+          width={500}
+          height={500}
+          className="w-1/3 h-full object-cover"
+        />
+        <Image
+          src="/image/hero5.jpeg"
+          alt="hero-image-2"
+          width={500}
+          height={500}
+          className="w-1/3 h-full object-cover"
+        />
+        <Image
+          src="/image/garm1.jpg"
+          alt="hero-image-3"
+          width={500}
+          height={500}
+          className="w-1/3 h-full object-cover"
+        />
       </div>
 
-      {/* Right Section (Two Side-by-Side Images) */}
-      <div className="w-3/5 h-full flex">
-        <Image src="/image/hero4.jpeg" alt="hero-image-1" width={500} height={500} className="w-1/2 h-full object-cover" />
-        <Image src="/image/hero5.jpeg" alt="hero-image-2" width={500} height={500} className="w-1/2 h-full object-cover" />
+      {/* Dark Overlay for better text contrast */}
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      {/* Centered Content */}
+      <div className="relative z-10 text-center text-white flex flex-col items-center justify-center w-full px-6">
+        <Image
+          src="/icons/hero.svg"
+          alt="hero-icon"
+          width={100}
+          height={100}
+          className="mx-auto mb-4"
+        />
+        <span className="block text-center text-white md:text-[3rem] text-[1.7rem]">
+          {"Rooted in Culture,\nStyled for Everyone."}
+        </span>
       </div>
 
+      {/* Bottom Centered Learn More Button */}
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+        <LearnMoreBtn
+          text="Learn More about Afrogarm"
+          borderColor="border-white"
+          textColor="text-white"
+          route="http://afrogarm.com"
+        />
+      </div>
     </section>
   );
 };
