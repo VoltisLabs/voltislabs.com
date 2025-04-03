20afd6720dc3a62287209df1b5795f60b047897c
rich <adukyerer@gmail.com>
2025-04-03 12:40:16 +0000

    Add products page with partner cards display

---
+++ src/app/products/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { partners } from '../data'
+import Card from '../products/components/card'
+
+const page = () => {
+    return (
+        <div className='pt-32 px-10 md:px-20 xl:px-0'>
+            <h1 className='text-white font-medium text-4xl'>Products</h1>
+            <p className='text-lg font-normal leading-7 mt-16 mb-20 text-white'></p>
+
+            <div className="grid grid-cols-1 gap-12 md:gap-24 py-10">
+                {partners.map((product, index) => (
+                    <Card key={index} title={product.message} description={product.description} image={product.img} link={product.link} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default page
