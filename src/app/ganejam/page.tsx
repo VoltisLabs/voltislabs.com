8013f3cb677bc95176a5d0e31c4bfc8011512dfe
AYOPELUMI <ayopelumi2014@gmail.com>
2025-05-06 06:31:15 -0700

    Tuesday tasks 6th-5-25

---
+++ src/app/ganejam/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='container mx-auto px-4 py-12 max-w-4xl text-white pt-20 flex justify-center items-center'>Coming soon</div>
+    )
+}
+
+export default page
