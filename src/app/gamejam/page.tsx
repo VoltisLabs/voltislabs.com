cc8f07d701c9cd6a48724391f0b7a5ec6870d480
AYOPELUMI <ayopelumi2014@gmail.com>
2025-05-06 06:37:54 -0700

    Tuesday tasks 6th-5-25

--- src/app/gamejam/page.tsx
+++ src/app/gamejam/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='container mx-auto px-4 py-12 max-w-4xl text-white pt-20 flex justify-center items-center'>Coming soon</div>
+        <div className='min-h-screen font-bold container mx-auto px-4 py-12 max-w-4xl text-white pt-20 flex justify-center items-center'>Coming soon</div>
     )
 }
 
