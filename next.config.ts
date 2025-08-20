254b7d0206f6704fa476457e0304f9f1a59fc244
Stanley <stanley.s@ihealthwellness.org>
2025-08-20 03:37:24 -0400

    fixed issues

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'eu-west-2.graphassets.com',
       },
-<<<<<<< HEAD
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-      },
-=======
      
->>>>>>> 0a613e5288c919432381b528b38f616da9907e30
     ],
   },
 };
